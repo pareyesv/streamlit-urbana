84a2fe694c850d67c6e20e935c98907578c5d7f2
egregorimar <egregorimar@gmail.com>
2021-07-04 20:29:26 +0200

    wip: interactive map

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-"""Demo app
 
+"""Demo app
 Ref:
 - https://docs.streamlit.io/en/stable/getting_started.html
 - https://deckgl.readthedocs.io/en/latest/gallery/geojson_layer.html
@@ -12,86 +12,85 @@ import pydeck as pdk
 import geopandas as gpd
 
 
-st.title("My first app")
+# st.title("My first app")
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(
-    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
-)
+# st.write("Here's our first attempt at using data to create a table:")
+# st.write(
+#     pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+# )
 
-df_trial = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+# df_trial = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
 
-df_trial
+# df_trial
 
-# another way to display df
-chart_data = df_trial
+# # another way to display df_trial
+# chart_data = df_trial
 
-st.line_chart(chart_data)
+# st.line_chart(chart_data)
 
-# map
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
-)
+# # map
+# map_data = pd.DataFrame(
+#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
+# )
 
-st.map(map_data)
+# st.map(map_data)
 
-# checkbox
-if st.checkbox("Show dataframe"):
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+# # checkbox
+# if st.checkbox("Show dataframe"):
+#     chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-    chart_data
+#     chart_data
 
-# selectbox
-option = st.selectbox("Which number do you like best?", df_trial["first column"])
+# # selectbox
+# option = st.selectbox("Which number do you like best?", df_trial["first column"])
 
-"You selected: ", option
+# "You selected: ", option
 
-# button
-left_column, right_column = st.beta_columns(2)
-pressed = left_column.button("Press me?")
-if pressed:
-    right_column.write("Woohoo!")
+# # button
+# left_column, right_column = st.beta_columns(2)
+# pressed = left_column.button("Press me?")
+# if pressed:
+#     right_column.write("Woohoo!")
 
-# expander
-expander = st.beta_expander("FAQ")
-expander.write("Here you could put in some really, really long explanations...")
+# # expander
+# expander = st.beta_expander("FAQ")
+# expander.write("Here you could put in some really, really long explanations...")
 
-# map geojson layer
-# ref: https://deckgl.readthedocs.io/en/latest/gallery/geojson_layer.html
-DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
-LAND_COVER = [
-    [[-123.0, 49.196], [-123.0, 49.324], [-123.306, 49.324], [-123.306, 49.196]]
-]
+# # map geojson layer
+# # ref: https://deckgl.readthedocs.io/en/latest/gallery/geojson_layer.html
+# DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
+# LAND_COVER = [
+#     [[-123.0, 49.196], [-123.0, 49.324], [-123.306, 49.324], [-123.306, 49.196]]
+# ]
 
-INITIAL_VIEW_STATE = pdk.ViewState(
-    latitude=49.254, longitude=-123.13, zoom=11, max_zoom=16, pitch=45, bearing=0
-)
+# INITIAL_VIEW_STATE = pdk.ViewState(
+#     latitude=49.254, longitude=-123.13, zoom=11, max_zoom=16, pitch=45, bearing=0
+# )
 
-polygon = pdk.Layer(
-    "PolygonLayer",
-    LAND_COVER,
-    stroked=False,
-    # processes the data as a flat longitude-latitude pair
-    get_polygon="-",
-    get_fill_color=[0, 0, 0, 20],
-)
+# polygon = pdk.Layer(
+#     "PolygonLayer",
+#     LAND_COVER,
+#     stroked=False,
+#     # processes the data as a flat longitude-latitude pair
+#     get_polygon="-",
+#     get_fill_color=[0, 0, 0, 20],
+# )
 
-geojson = pdk.Layer(
-    "GeoJsonLayer",
-    DATA_URL,
-    opacity=0.8,
-    stroked=False,
-    filled=True,
-    extruded=True,
-    wireframe=True,
-    get_elevation="properties.valuePerSqm / 20",
-    get_fill_color="[255, 255, properties.growth * 255]",
-    get_line_color=[255, 255, 255],
-)
+# geojson = pdk.Layer(
+#     "GeoJsonLayer",
+#     DATA_URL,
+#     opacity=0.8,
+#     stroked=False,
+#     filled=True,
+#     extruded=True,
+#     wireframe=True,
+#     get_elevation="properties.valuePerSqm / 20",
+#     get_fill_color="[255, 255, properties.growth * 255]",
+#     get_line_color=[255, 255, 255],
+# )
 
-r = pdk.Deck(layers=[polygon, geojson], initial_view_state=INITIAL_VIEW_STATE)
-st.pydeck_chart(r)
-
+# r = pdk.Deck(layers=[polygon, geojson], initial_view_state=INITIAL_VIEW_STATE)
+# st.pydeck_chart(r)
 
 
 ########################################################################################################
@@ -113,4 +112,22 @@ df.set_index("Tag", inplace=True)
 
 df_geo["Target"] = df[VARIABLE_TO_PREDICT]
 
-df_geo
+
+# COVER_BCN = [
+#     [[2.183787, 41.507757], [2.151772 ,41.283110], [2.305666, 41.407666], [1.984145, 41.385002]]
+# ]
+
+INITIAL_VIEW_STATE_BCN = pdk.ViewState(
+    latitude=41.39, longitude=2.15, zoom=10, max_zoom=16, pitch=45, bearing=0
+)
+
+geojson_bcn = pdk.Layer("GeoJsonLayer",
+    data=df_geo,
+    get_fill_color=[255, 255, 255],
+    get_line_color=[0,0,0],
+    get_elevation="Target",
+    # initial_view_state=COVER_BCN
+)
+
+bcn = pdk.Deck(layers=[geojson_bcn], initial_view_state=INITIAL_VIEW_STATE_BCN)
+st.pydeck_chart(bcn)
