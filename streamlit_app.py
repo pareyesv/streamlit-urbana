89d058747aef8b0cc8c3cdb1ce837abf0f76d3c4
Patricio Reyes <patricio.reyes@bsc.es>
2021-07-24 17:38:58 +0200

    fix: fix caching in get_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,22 @@ import pandas as pd
 import pydeck as pdk
 from __version__ import version
 
-st.title("InsideAirBnB data for Barcelona")
+YEAR = 2017
+MONTH = 5
+URL_GEO = "https://raw.githubusercontent.com/egregorimar/urbana/master/data/interim/sections_geo.json"
+URL_DATA = "https://raw.githubusercontent.com/egregorimar/urbana/master/data/interim/sections_{}_{:02d}.csv".format(
+    YEAR, MONTH
+)
+INITIAL_LAT = 41.38879
+INITIAL_LON = 2.15899
+CITY_NAME = "Barcelona"
 
+
+st.title(f"InsideAirBnB data for {CITY_NAME}")
 
 # data
 @st.cache(allow_output_mutation=True)
-def get_data():
+def get_data(url_geo: str, url_data: str) -> dict:
     """Load and merge data.
 
     Returns:
@@ -26,17 +36,10 @@ def get_data():
     import geopandas as gpd
     import json
 
-    YEAR = 2017
-    MONTH = 5
-    URL_GEO = "https://raw.githubusercontent.com/egregorimar/urbana/master/data/interim/sections_geo.json"
-    URL_DATA = "https://raw.githubusercontent.com/egregorimar/urbana/master/data/interim/sections_{}_{:02d}.csv".format(
-        YEAR, MONTH
-    )
-
-    df_geo = gpd.read_file(URL_GEO)
+    df_geo = gpd.read_file(url_geo)
     df_geo.set_index("Tag", inplace=True)
 
-    df = pd.read_csv(URL_DATA)
+    df = pd.read_csv(url_data)
     df.set_index("Tag", inplace=True)
 
     full_df = df_geo.merge(df, on="Tag")
@@ -44,9 +47,9 @@ def get_data():
     return {"describe": df.describe(), "geojson": json.loads(full_df.to_json())}
 
 
-bcn_data = get_data()
-bcngeojson = bcn_data["geojson"]
-features = bcn_data["describe"].columns.values
+city_data = get_data(url_geo=URL_GEO, url_data=URL_DATA)
+city_geojson = city_data["geojson"]
+features = city_data["describe"].columns.values
 
 # select features for elevation & color
 option_elevation = st.selectbox(
@@ -57,26 +60,31 @@ option_color = st.selectbox(
 )
 
 # plot
-BCN_INITIAL_VIEW_STATE = pdk.ViewState(
-    latitude=41.38879, longitude=2.15899, zoom=11, max_zoom=16, pitch=45, bearing=0
+INITIAL_VIEW_STATE = pdk.ViewState(
+    latitude=INITIAL_LAT,
+    longitude=INITIAL_LON,
+    zoom=11,
+    max_zoom=16,
+    pitch=45,
+    bearing=0,
 )
 
-bcngeojson = pdk.Layer(
+city_geojson = pdk.Layer(
     "GeoJsonLayer",
-    data=bcngeojson,
+    data=city_geojson,
     # opacity=0.8,
     stroked=False,
     filled=True,
     extruded=True,
     wireframe=True,
     get_elevation=f"properties.{option_elevation}",
-    elevation_scale=bcn_data["describe"].loc["max"].max()
-    / bcn_data["describe"][option_elevation]["max"],
-    get_fill_color=f"""[255, (1 - properties.{option_color} / {bcn_data["describe"][option_color]["max"]}) * 255, 255]""",
+    elevation_scale=city_data["describe"].loc["max"].max()
+    / city_data["describe"][option_elevation]["max"],
+    get_fill_color=f"""[255, (1 - properties.{option_color} / {city_data["describe"][option_color]["max"]}) * 255, 255]""",
     get_line_color=[255, 255, 255],
 )
 
-r = pdk.Deck(layers=[bcngeojson], initial_view_state=BCN_INITIAL_VIEW_STATE)
+r = pdk.Deck(layers=[city_geojson], initial_view_state=INITIAL_VIEW_STATE)
 st.pydeck_chart(r)
 
 # credits and version
